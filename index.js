d11f1f19a50f8f94072942148a56f3f08d3cf47d
ssihvola <sihvola@protonmail.com>
2023-10-30 13:59:20 +0200

    Complete rock, paper, scissors gameplay

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
   let randomInteger = Math.floor(Math.random() * 3)
-  
+
   if (randomInteger === 0) {
     return 'rock'
   } else if (randomInteger === 1) {
@@ -10,4 +10,42 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice())
+function getPlayerChoice() {
+  let randomInteger = Math.floor(Math.random() * 3)
+
+  if (randomInteger === 0) {
+    return 'rock'
+  } else if (randomInteger === 1) {
+    return 'paper'
+  } else {
+    return 'scissors'
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  let lowerCasedPlayerSelection = playerSelection.toLowerCase()
+
+  if (lowerCasedPlayerSelection === 'rock' && computerSelection === 'scissors') {
+    return 'You win! Rock beats scissors.'
+  } else if (lowerCasedPlayerSelection === 'rock' && computerSelection === 'paper') {
+    return 'You lose! Paper beats rock.'
+  } else if (lowerCasedPlayerSelection === 'paper' && computerSelection === 'rock') {
+    return 'You win! Paper beats rock.'
+  } else if (lowerCasedPlayerSelection === 'paper' && computerSelection === 'scissors') {
+    return 'You lose! Scissors beat paper.'
+  } else if (lowerCasedPlayerSelection === 'scissors' && computerSelection === 'paper') {
+    return 'You win! Scissors beat paper.'
+  } else if (lowerCasedPlayerSelection === 'scissors' && computerSelection === 'rock') {
+    return 'You lose! Rock beats scissors.'
+  } else {
+    return 'Tie!'
+  }
+}
+
+const playerSelection = getPlayerChoice()
+const computerSelection = getComputerChoice()
+
+console.log(playerSelection)
+console.log(computerSelection)
+
+console.log(playRound(playerSelection, computerSelection))
