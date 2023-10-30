84094282b75cb75664fc502f7e07ce7c8fe357a4
ssihvola <sihvola@protonmail.com>
2023-10-30 13:44:33 +0200

    add function getComputerChoice

---
+++ index.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let randomInteger = Math.floor(Math.random() * 3)
+  
+  if (randomInteger === 0) {
+    return 'rock'
+  } else if (randomInteger === 1) {
+    return 'paper'
+  } else {
+    return 'scissors'
+  }
+}
+
+console.log(getComputerChoice())
