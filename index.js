ea97ad4e316a13b88563e4a20d24f66285f28183
ssihvola <sihvola@protonmail.com>
2023-10-30 14:42:04 +0200

    add points to game

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerPoints = 0
+let computerPoints = 0
+
 function getComputerChoice() {
   let randomInteger = Math.floor(Math.random() * 3)
 
@@ -17,10 +20,12 @@ function playRound(playerSelection, computerSelection) {
     paper: 'rock',
     scissors: 'paper'
   }
-  
+
   if (winningCombos[player] === computerSelection) {
+    playerPoints++
     return `You win! ${playerSelection} beats ${computerSelection}`
   } else if (winningCombos[computerSelection] === player) {
+    computerPoints++
     return `You lose! ${computerSelection} beats ${playerSelection}`
   } else {
     return 'Tie!'
@@ -32,7 +37,15 @@ function game() {
     const playerSelection = prompt("rock, paper or scissors?")
     const computerSelection = getComputerChoice()
     console.log(playRound(playerSelection, computerSelection))
+    console.log(playerPoints)
+    console.log(computerPoints)
   }
+
+  if (playerPoints > computerPoints) {
+    console.log('you win. congratulations.')
+  } else {
+    console.log('you lose.')
+  } 
 }
 
 game()
